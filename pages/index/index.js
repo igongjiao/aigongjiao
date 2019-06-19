064df178d300b7ddcc5689f4a1ed67999772f879
Chencpy6 <253545375@qq.com>
2019-06-19 14:43:39 +0800

    Revert "start"
    This reverts commit de7071e791974a4bac9a7b5bd68cd26d0f450b02.

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,33 @@ const app = getApp()
 Page({
   data: {
     city:"武汉",
+    array:[
+      {
+        id:0,
+        text:"推荐",
+    },
+      {
+        id: 1,
+        text: "收藏",
+    }, {
+        id: 2,
+        text: "历史",
+    }],
+    image:[{
+      src:'../../image/main_1.png',
+      page:'../logs/logs'
+      },
+      { 
+        src: '../../image/main_2.png' ,
+        page:'../logs/logs'
+        },
+      { 
+        src: '../../image/main_3.png',
+        page:'../logs/logs'
+         },
+      { src: '../../image/main_4.png' ,
+        page:'../logs/logs'
+      }],
     bus:[
       {
         bus_number:"781",destination:"武汉大学",station:"珞瑜东路佳园站",accuracy:"",wait_time:"45"
@@ -30,7 +57,7 @@ Page({
       {
         bus_number: "781", destination: "武汉大学", station: "珞瑜东路佳园站", accuracy: "", wait_time: "45"
       }],
-    show:true,
+    num:-1,
     scrollTop:300,
   },
   //事件处理函数
@@ -39,17 +66,31 @@ Page({
       url: '../logs/logs'
     })
   },
+  Location: function(){
+  //  this.setData({
+  //    city:"上海"
+  //  })
+    //  wx.navigateTo({
+    //    url: '../',
+    //  })
+  },
   Search: function(){
     wx.navigateTo({
       url: '../search/search',
     })
   },
-  Switch_show:function(e){
-    var show_hidden=this.data.show
-    this.setData({
-       show:!show_hidden
+  Button_jump: function(){
+    var url = e.currentTarget.dataset.index
+    console.log(url)
+    wx.navigateTo({
+      url: url,
     })
-    console.log(show_hidden)
+  },
+  Change_color:function(e){
+    var index = parseInt(e.currentTarget.dataset.index)
+    this.setData({
+      num: index
+    })
   },
 
   
